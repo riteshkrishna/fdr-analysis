1a0719e0259974228cbb097f2dce9b88ff100755
davidcwedge <davidcwedge@7070a3f2-3587-11de-8ef0-0f53c1b7e458>
2010-02-05 11:07:00 +0000

    Text in the help pages improved

--- static_pages/help_page.pl
+++ static_pages/help_page.pl
@@ -29,28 +29,36 @@ if($subject eq "N_terminal_analysis")
 	print header,
 	start_html({-title=>'Help: N-terminal analysis','-onload' => 'focus();'}),
 	h2('Help: N-terminal analysis'),
-	"If the N-terminal analysis box is checked, the analysis will determine the fraction of identifications that are the N-terminal most peptide of the protein (based on the start position reported by each of the different search engines).";
+	"If the N-terminal analysis box is checked, the analysis will include an N-terminal analysis, which will determine the number of identifications that are the N-terminal most peptide of the protein (based on the start position reported by each of the different search engines).";
 }
 elsif($subject eq "decoy_tag")
 {
 	print header,
 	start_html('Help: decoy tags'),
 	h2('Help: decoy tags'),
-	"This is the 'tag' used to mark which proteins are from the decoy search.  This is required when a concatenated search was performed.  It can be included if a tag was used in the decoy database of a search done separately against target and decoy databases.  It is not required for a Mascot decoy search.  For example if the target database has accession >P12345 and the equivalent decoy protein is >REVERSE_P12345 then the tag is 'REVERSE_'.  <B>Please note that the tag must be at the start of the protein accession and have no spaces between it and the protein</B>.";
+	"This is the 'tag' used to mark the proteins which are decoys. For example, if the target database has a protein with accession number >P12345 and the equivalent decoy protein is >REVERSE_P12345 then the tag is 'REVERSE_'. It is very important for the FDR calculations that this tag is entered correctly.";
 }
+elsif($subject eq "decoy_ratio")
+{
+	print header,
+	start_html('Help: decoy size'),
+	h2('Help: decoy size'),
+	"This is the size of the decoy database, relative to the target database. If the 2 databases are of equal size this value should be 1. It is important for the FDR calculations that this value is entered is correctly.";
+}
+
 elsif($subject eq "expectation_value")
 {
 	print header,
 	start_html('Help: expectation value'),
 	h2('Help: expectation value'),
-	"The maximum expectation value is the cut-off for one of the resulting plots. This plot is useful for investigating the results.";
+	"The maximum expectation value determines which identifications will be included in the rank plot and start position plot.";
 }
 elsif($subject eq "FDR")
 {
 	print header,
 	start_html('Help: FDR'),
 	h2('Help: false discovery rate'),
-	"The false discovery rate (FDR) you would like the results for.";
+	"Results will be reported up to the false discovery rate (FDR) entered here.";
 }
 elsif($subject eq "combine_results")
 {
@@ -59,63 +67,19 @@ elsif($subject eq "combine_results")
 	h2('Help: combining results'),
 	"If 'combine results' is true, the algorithm described in <i>Jones et al. Proteomics (2009) 9: 1220-9</i> is employed to combine the results of two or more searches from the different search engines.  This search can take a few extra minutes (depending on file sizes), so please be patient.";
 }
-elsif($subject eq "mascot_search")
+elsif($subject eq "input_files")
 {
 	print header,
-	start_html('Help: Mascot search types'),
-	h2('Help: Mascot search types'),
-	"There are 3 types of decoy search: <ul><li>A concatenated target/decoy whereby the target and decoy databases were joined and one search was performed.<li>A Mascot decoy search whereby the 'decoy' checkbox was selected in the Mascot search.<li>Two separate searches, one on the target database and a second on the decoy database.";
+	start_html('Help: Input files'),
+	h2('Help: Input files'),
+	"Uploaded files should be the results of a concatenated target/decoy search, in which the target and decoy databases were joined and one search was performed.";
 }
-elsif($subject eq "mascot_decoy_files")
+elsif($subject eq "file_types")
 {
 	print header,
-	start_html('Help: Mascot decoy and concatenated files'),
-	h2('Help: Mascot decoy and concatenated files'),
-	"A Mascot '.dat' file is expected here. This upload box should be used to upload the results of searching aganist a decoy, concatenated or reverse database. In the last case, the results of a forward search should be uploaded in the box below.";
-}
-elsif($subject eq "mascot_forward_files")
-{
-	print header,
-	start_html('Help: Mascot forward files'),
-	h2('Help: Mascot forward files'),
-	"A Mascot '.dat' file is expected here. This upload box should only be used to upload the results of a search run solely on the target database, without decoys. The results of a decoy (or reverse) search should be uploaded in the box above.";
-}
-elsif($subject eq "OMSSA_search")
-{
-	print header,
-	start_html('Help: OMSSA search types'),
-	h2('Help: OMSSA search types'),
-	"There are 2 types of decoy search for OMSSA:<ul><li>A single search was performed on a concatenated target/decoy database containing both targets and decoys.<li>Two separate searches were performed, one on the target database and one on the decoy database.</ul>";
-}
-elsif($subject eq "OMSSA_version")
-{
-	print header,
-	start_html('Help: OMSSA version'),
-	h2('Help: OMSSA version'),
-	"Enter the OMSSA version number used to perform the searches. Run 'omssacl -version' to check your software version.";
-}
-elsif($subject eq "OMSSA_decoy_files")
-{
-	print header,
-	start_html('Help: OMSSA reverse and concatenated files'),
-	h2('Help: OMSSA reverse and concatenated files'),
-	"An OMSSA '.csv' results file is expected here. This upload box should be used to upload the results of searching aganist a concatenated or reverse database. In the second case, the results of a forward search should be uploaded in the box below.";
-}
-elsif($subject eq "OMSSA_forward_files")
-{
-	print header,
-	start_html('Help: OMSSA forward files'),
-	h2('Help: OMSSA forward files'),
-	"An OMSSA '.csv' results file is expected here. This upload box should only be used to upload the results of a search run solely on the target database, without decoys. The results of a decoy (or reverse) search should be uploaded in the box above.";
-}
-elsif($subject eq "Tandem_search")
-{
-	print header,
-	start_html('Help: X!Tandem search types'),
-	h2('Help: X!Tandem search types'),
-	"There are 2 types of decoy search for X!Tandem:<ul><li>A single search was performed on a concatenated target/decoy database containing both targets and decoys.<li>Two separate searches were performed, one on the target database and one on the decoy database.</ul>";
-
-
+	start_html('Help: Input file types'),
+	h2('Help: Input file types'),
+	"The accepted files types are:  Mascot .dat files; OMSSA .csv files; <span style='white-space:nowrap'>X!Tandem</span> .xml files; .mzident files from either Mascot, OMSSA or <span style='white-space:nowrap'>X!Tandem</span>.";
 }
 elsif($subject eq "Tandem_version")
 {
@@ -123,20 +87,6 @@ elsif($subject eq "Tandem_version")
 	start_html('Help: X!Tandem version'),
 	h2('Help: X!Tandem version'),
 	"Enter the X!Tandem version number used to perform the searches. Run 'tandem' without any command line parameters to check your software version.";
-}
-elsif($subject eq "Tandem_decoy_files")
-{
-	print header,
-	start_html('Help: X!Tandem reverse and concatenated files'),
-	h2('Help: X!Tandem reverse and concatenated files'),
-	"An X!Tandem '.csv' results file is expected here. This upload box should be used to upload the results of searching aganist a concatenated or reverse database. In the second case, the results of a forward search should be uploaded in the box below.";
-}
-elsif($subject eq "Tandem_forward_files")
-{
-	print header,
-	start_html('Help: X!Tandem forward files'),
-	h2('Help: X!Tandem forward files'),
-	"An X!Tandem '.xml' results file is expected here. This upload box should only be used to upload the results of a search run solely on the target database, without decoys. The results of a decoy (or reverse) search should be uploaded in the box above.";
 }
 elsif($subject eq "parent_mass_type")
 {
@@ -152,6 +102,14 @@ elsif($subject eq "fragment_mass_type")
 	h2('Help: mass types'),
 	"There are 2 possible atomic mass types used during search. The monoisotopic mass is the mass of the most commonly occurring isotope, the average mass is averaged across all isotopes (weighted by distribution).";
 }
+elsif($subject eq "tolerance")
+{
+	print header,
+	start_html('Help: peptide mass tolerance'),
+	h2('Help: peptide mass tolerance'),
+	"A peptide mass identified by mass spectometry may not exactly match the theoretical mass. The mass tolerance sets the level of 'error' allowed between the theoretical and observed masses when making a positive identification. Mass tolerances may be set in Daltons or in parts per million, and separate tolerances are set for parent and fragment ions.";
+}
+
 elsif($subject eq "select_enzyme")
 {
 	print header,
@@ -164,7 +122,7 @@ elsif($subject eq "missed_cleavages")
 	print header,
 	start_html('Help: missed cleavages'),
 	h2('Help: missed cleavages'),
-	"Enzymes cleave proteins at specific amino acid residues. Howvever, the cleavage of proteins is often incomplete. Enter a maximum number of missed cleavages allowed in the resultant peptides.";
+	"Enzymes cleave proteins at specific amino acid residues. However, the cleavage of proteins is often incomplete. Enter a maximum number of missed cleavages to be allowed in the resultant peptides.";
 }
 elsif($subject eq "fixed_mods")
 {
